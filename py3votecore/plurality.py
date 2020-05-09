48cd4bbe4f376a9c40ded48275573aaf9e3e8164
Gabriele Calvo <gcalvo87@gmail.com>
2020-05-10 00:33:51 +0100

    added random_seed to Plurality

--- py3votecore/plurality.py
+++ py3votecore/plurality.py
@@ -3,5 +3,5 @@ from .plurality_at_large import PluralityAtLarge
 
 
 class Plurality(AbstractSingleWinnerVotingSystem):
-    def __init__(self, ballots, tie_breaker=None):
-        super().__init__(ballots, PluralityAtLarge, tie_breaker=tie_breaker)
+    def __init__(self, ballots, tie_breaker=None, random_seed=None):
+        super().__init__(ballots, PluralityAtLarge, tie_breaker=tie_breaker, random_seed=random_seed)
